0dbdf15b0d9805af30b1df469ba975eae1f125b5
Leonardo Reis <accs.lndrs@gmail.com>
2023-01-23 10:20:20 -0300

    feat: added custom logger with colored message for each log level (log, warn, error)

--- server/main.cpp
+++ server/main.cpp
@@ -1,25 +1,29 @@
 #include <iostream>
 #include "./comms/socket/server_socket.hpp"
-#define MASTER_SOCKET_PORT 8080
+#define MASTER_SOCKET_PORT 8081
 #define MASTER_SOCKET_QUEUE_SIZE 5
+
+#include "../commons/logger/logger.hpp"
 
 using namespace std;
 int main()
 {
-    cout<<"starting server...\n";
+    logger::log("starting server...");
 
     Socket master_socket = Socket(MASTER_SOCKET_PORT, MASTER_SOCKET_QUEUE_SIZE);
 
     if (int err = master_socket.bind_and_listen() < 0) {
-        cout<<"ERROR on binding\n";
+        logger::error("error on binding");
         return err;
     }
 
-
     Socket slave_socket = master_socket.accept_connection();
-    slave_socket.read_packet();
+    int n = slave_socket.read_packet();
     packet p = slave_socket.get_buffer();
-    cout<<"message received:"<<p._payload<<"\nof type:"<<p.type<<"\n";
+    cout<<"------\n";
+    cout<<"message received: "<<p._payload<<"\n";
+    cout<<"of type: "<<p.type<<"\n";
+    cout<<"number of bytes: "<<n<<"\n";
 
 	master_socket.close_connection();
 	slave_socket.close_connection();
